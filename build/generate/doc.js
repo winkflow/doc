1fb6394fa37bdbba7e4fa0afe4dc396501000fa8
Victor <lmslovecs@gmail.com>
2018-11-11 17:42:50 +0800

    feat(root): fit winkflow doc

--- build/generate/doc.js
+++ build/generate/doc.js
@@ -4,12 +4,21 @@ const generateNavHTML = require("../doc-plugin/nav-html");
 const generateDocHTML = require("../doc-plugin/doc-html");
 const generateSubNavHTML = require("../doc-plugin/subnav-html");
 
+     //generateDoc("markdown/doc", "SUMMARY.md", "dist", "doc");
 module.exports = function(baseURL, navFileName, releasePath, prefixPath) {
+
+  ////SUMMARY.MD
   const navFile = fs.readFileSync(`${baseURL}/${navFileName}`);
+
+  ////模板
   const templateFile = fs.readFileSync(`${releasePath}/doc.html`);
+
+  ///SUMMARY.MD获得ast和list
   const navStructure = generateNavStructure(navFile, prefixPath);
   const ast = navStructure.ast;
   const listOfNav = navStructure.list;
+
+
   listOfNav.forEach((nav, index) => {
     const listOfPath = nav.path.split("/");
     listOfPath.pop();
@@ -17,9 +26,13 @@ module.exports = function(baseURL, navFileName, releasePath, prefixPath) {
     fs.ensureDir(`${releasePath}/${prefixPath}/${ensurePath}`).then(() => {
       try {
         const docFile = fs.readFileSync(`${baseURL}/${nav.path}`);
+
+
         const navHTML = generateNavHTML(ast, nav.path);
         const docHTML = generateDocHTML(docFile);
         const subNavHTML = generateSubNavHTML(listOfNav[index - 1], listOfNav[index + 1]);
+
+        ////替换模板中的
         const targetFile = String(templateFile).replace("$nav", navHTML).replace("$doc", docHTML).replace("$subnav", subNavHTML);
         const targetPath = `${releasePath}/${nav.url}`;
         fs.writeFileSync(`${targetPath}`, targetFile);
